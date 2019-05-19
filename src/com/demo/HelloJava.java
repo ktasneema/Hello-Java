14f7d9f1f1d04874bd8fb8cb22dd2a080135905a
Tasneema <ktasneema@gmail.com>
2019-05-20 00:33:25 +0530

    first eclipse to git

---
+++ src/com/demo/HelloJava.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class HelloJava {
+
+	public HelloJava() {
+		System.out.println("default ctor");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+
+	}
+
+}
